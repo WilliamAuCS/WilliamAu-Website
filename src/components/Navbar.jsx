b90c45fb051369436ef9f58dabc805e53097cf1b
William Au <WilliamAuCS@Gmail.com>
2019-06-09 14:38:01 -0700

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-toggleable-md">
+                <button className="navbar-toggler navbar-toggler-right" aria-expanded="false" aria-controls="navbarCollapse" aria-label="Toggle navigation" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="container">
+                    <Link className="navbar-brand" to="/">William Au</Link>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Navbar
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+import Jumbotron from '../components/Jumbotron.jsx';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Jumbotron title="Contact" subtitle="put contact information here!" />
+                <div className="container">
+                    <h2>About</h2>
+                        <p>
+                            This area is filled with random text I am currently writing on the spot. Hopefully this tutorial goes well so I can get experience being a front-end developer
+                            and see if it is something I might consider doing in the future. I currently enjoy back-end developing more, however I believe experience in both fields is 
+                            crutial for a programmer to be at the best of his ability, and provide the best service for his employer. 
+                        </p>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Contact
