a3616b5eed2e4641ab4f6840b632b58a29c5bcf1
William Au <WilliamAuCS@gmail.com>
2019-06-12 10:36:43 -0700

    Added portrait and arranging item locations

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './SideBar.css';
+
+class SideBar extends Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <a className="pic" a/>
+                <div className="info">
+                    <h3>William Au</h3>
+
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default SideBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
 import Jumbotron from '../components/Jumbotron.jsx';
+import SideBar from '../components/SideBar.jsx';
+import './Home.css';
 
 class Home extends Component {
     render() {
@@ -9,13 +11,14 @@ class Home extends Component {
             <div>
                 <Navbar />
                 <Jumbotron title="Welcome" subtitle="Bachelor of Science, Computer Science" />
-                <div className="container">
-                    <h2>Welcome</h2>
-                        <p>
-                            This area is filled with random text I am currently writing on the spot. Hopefully this tutorial goes well so I can get experience being a front-end developer
-                            and see if it is something I might consider doing in the future. I currently enjoy back-end developing more, however I believe experience in both fields is 
-                            crutial for a programmer to be at the best of his ability, and provide the best service for his employer. 
-                        </p>
+                <SideBar />
+                <div className="bio">
+                    <p>
+                        <h2>Welcome</h2>
+                        This area is filled with random text I am currently writing on the spot. Hopefully this tutorial goes well so I can get experience being a front-end developer
+                        and see if it is something I might consider doing in the future. I currently enjoy back-end developing more, however I believe experience in both fields is
+                        crutial for a programmer to be at the best of his ability, and provide the best service for his employer.
+                    </p>
                 </div>
                 <Footer />
             </div>
