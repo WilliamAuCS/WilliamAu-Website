0af216e829e6d2f5f652ea86545d4e593ca217fe
William Au <WilliamAuCS@gmail.com>
2019-07-03 16:32:16 -0700

    Completed Project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,49 +13,48 @@ class Projects extends Component {
                     <Navbar />
                     <Jumbotron title="My Projects" subtitle="Independent Projects" />
                     <div className="container">
-                        <div>
-                            <p className="overview">
-                                <h1>Projects</h1>
-                                The projects listed below are projects completed independently from my course work. Many are projects completed
-                                in a team of two or more programmers, however there are also many completed myself. Below each project is a brief
-                                description, including a screenshot of the actual project. If you wish to learn more, you may click on the project
+                        <p className="overview">
+                            <h1>Projects</h1>
+                            The projects listed below are projects completed independently from my course work. Many are projects completed
+                            in a team of two or more programmers, however there are also many completed myself. Below each project is a brief
+                            description, including a screenshot of the actual project. If you wish to learn more, you may click on the project
                                 name and a more in-depth explanation will be given. <br />
-                                (If you are not redirected, the page is still in progress and will be up soon!)
+                            (If you are not redirected, the page is still in progress and will be up soon!)
                             </p>
-                            <div className="projectSideBar">
+                        <div className="projectList">
 
-                                <div className="project1">
-                                <img src={require('../images/Mysteria1.PNG')} height="200" width="200" alt="Mysteria" 
-                                title="Example picture of my in progress Mysteria RPG game"/>
-                                </div>
-
-                                <img src={require('../images/object_cube.png')} height="200" width="200" alt="Cube Runner" 
-                                title="Example picture of my first completed game"/>
-
-                                <img src={require('../images/react-logo.png')} height="200" width="200" alt="My Website" 
-                                title="I used the JavaScript library React to complete this website"/>
-
-                            </div>
-                            <div className="projectList">
-                                <p className="project1">
+                            <div className="projectFormat">
+                                <img id="picPadding" src={require('../images/Mysteria1.PNG')} height="200" width="200" alt="Mysteria"
+                                    title="Example picture of my in progress Mysteria RPG game" align="left" />
+                                <p>
                                     <Link to="/mysteria"><h2>Mysteria</h2></Link>
 
                                     This project was my first major game created using the Unity Engine. This game is an RPG style, tile based game
                                     allowing the player to make decisions that will help progress the storyline, as well as make NPC friends.
-                                </p>
-                                <p className="project2">
+                                    </p>
+                            </div>
+
+                            <div className="projectFormat">
+                                <img id="picPadding" src={require('../images/object_cube.png')} height="200" width="200" alt="Cube Runner"
+                                    title="Example picture of my first completed game" align="left" />
+                                <p>
                                     <Link to="/cuberunner"><h2>Cube Runner</h2></Link>
 
                                     This was my first Unity project. Starting off, I used many tutorials learning how to utilize the Unity rigid body
                                     system and implimenting physics. The game is playable with multiple complete levels, challenging even the best players.
-                                </p>
-                                <p className="project3">
+                                    </p>
+                            </div>
+
+                            <div className="projectFormat">
+                                <img id="picPadding" src={require('../images/react-logo.png')} height="200" width="200" alt="My Website"
+                                    title="I used the JavaScript library React to complete this website" align="left" />
+                                <p>
                                     <Link to="/myWebsite"><h2>This Website</h2></Link>
 
                                     This is the website you are currently looking at. I created this website using the JavaScript React library. Creating
                                     this website was a challenge at first, for it was the first time experience the front-end of programming, however the
                                     concept quickly became familiar. This website will continue to be in development as I learn more about the React library.
-                                </p>
+                                    </p>
                             </div>
                         </div>
                     </div>
