285f5aa862ec26ee81d29b10d34110e12ec1133e
William Au <WilliamAuCS@gmail.com>
2019-07-03 15:33:12 -0700

    Project page formatting changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,22 +2,34 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
 import Jumbotron from '../components/Jumbotron.jsx';
+import './Contact.css';
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                <Navbar />
-                <Jumbotron title="Contact" subtitle="put contact information here!" />
-                <div className="container">
-                    <h2>About</h2>
+                <div className="contactBackground">
+                    <Navbar />
+                    <Jumbotron title="Contact" subtitle="put contact information here!" />
+                    <div className="contact">
+                        <h2>Contact</h2>
                         <p>
-                            This area is filled with random text I am currently writing on the spot. Hopefully this tutorial goes well so I can get experience being a front-end developer
-                            and see if it is something I might consider doing in the future. I currently enjoy back-end developing more, however I believe experience in both fields is 
-                            crutial for a programmer to be at the best of his ability, and provide the best service for his employer. 
+                            Feel free to contact me if you have any questions. Thanks!
+                            <br /><br /><br />
+                            Email: WilliamAuCS@Gmail.com 
+                            <br />
+                            Phone: (909) 362-3303
+                            <br />
+                            Linked-In: https://www.linkedin.com/in/william-au-964ab616b/ 
+                            <a href="https://www.linkedin.com/in/william-au-964ab616b/" target="_blank"> Link </a>
+                            <br />
+                            GitHub: https://github.com/WilliamAuCS 
+                            <a href="https://github.com/WilliamAuCS" target="_blank"> Link </a>
+                            <br />
                         </p>
+                    </div>
+                    <Footer />
                 </div>
-                <Footer />
             </div>
         );
     }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,37 +12,52 @@ class Projects extends Component {
                 <div className="projectBackground">
                     <Navbar />
                     <Jumbotron title="My Projects" subtitle="Independent Projects" />
-                    <div className="projectSideBar">
-                        <div id="project1Image">
-                            <p>Mysteria</p>
-                        </div>
-                        <div id="project2Image">
-                            <p>Cube Runner</p>
-                        </div>
-                    </div>
-                    <div className="projectCushBar"> </div>
-                    <div className="projects">
-                        <p className="overview">
-                            <h2>Projects</h2>
-                            The projects listed below are projects completed independently from my course work. Many are projects completed
-                            in a team of two or more programmers, however there are also many completed myself. Below each project is a brief
-                            description, including a screenshot of the actual project. If you wish to learn more, you may click on the project
-                            name and a more in-depth explanation will be given. <br />
-                            (If you are not redirected, the page is still in progress and will be up soon!)
-                        </p>
-                        <p className="project1">
-                            <Link to="/mysteria"><h2>Mysteria</h2></Link>
-                            
-                            This project was my first major game created using the Unity Engine. This game is an RPG style, tile based game
-                            allowing the player to make decisions that will help progress the storyline, as well as make NPC friends. 
-                        </p>
-                        <p className="project2">
-                            <Link to="/cuberunner"><h2>Cube Runner</h2></Link>
+                    <div className="container">
+                        <div>
+                            <p className="overview">
+                                <h1>Projects</h1>
+                                The projects listed below are projects completed independently from my course work. Many are projects completed
+                                in a team of two or more programmers, however there are also many completed myself. Below each project is a brief
+                                description, including a screenshot of the actual project. If you wish to learn more, you may click on the project
+                                name and a more in-depth explanation will be given. <br />
+                                (If you are not redirected, the page is still in progress and will be up soon!)
+                            </p>
+                            <div className="projectSideBar">
 
-                            This was my first Unity project. Starting off, I used many tutorials learning how to utilize the Unity rigid body
-                            system and implimenting physics. The game is playable with multiple complete levels, challenging even the best players. 
-                            What is going on here bruj
-                        </p>
+                                <div className="project1">
+                                <img src={require('../images/Mysteria1.PNG')} height="200" width="200" alt="Mysteria" 
+                                title="Example picture of my in progress Mysteria RPG game"/>
+                                </div>
+
+                                <img src={require('../images/object_cube.png')} height="200" width="200" alt="Cube Runner" 
+                                title="Example picture of my first completed game"/>
+
+                                <img src={require('../images/react-logo.png')} height="200" width="200" alt="My Website" 
+                                title="I used the JavaScript library React to complete this website"/>
+
+                            </div>
+                            <div className="projectList">
+                                <p className="project1">
+                                    <Link to="/mysteria"><h2>Mysteria</h2></Link>
+
+                                    This project was my first major game created using the Unity Engine. This game is an RPG style, tile based game
+                                    allowing the player to make decisions that will help progress the storyline, as well as make NPC friends.
+                                </p>
+                                <p className="project2">
+                                    <Link to="/cuberunner"><h2>Cube Runner</h2></Link>
+
+                                    This was my first Unity project. Starting off, I used many tutorials learning how to utilize the Unity rigid body
+                                    system and implimenting physics. The game is playable with multiple complete levels, challenging even the best players.
+                                </p>
+                                <p className="project3">
+                                    <Link to="/myWebsite"><h2>This Website</h2></Link>
+
+                                    This is the website you are currently looking at. I created this website using the JavaScript React library. Creating
+                                    this website was a challenge at first, for it was the first time experience the front-end of programming, however the
+                                    concept quickly became familiar. This website will continue to be in development as I learn more about the React library.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <Footer />
