2ddfffae4e317f2b48f24cb181e3975bbb1d5341
William Au <WilliamAuCS@Gmail.com>
2019-12-21 19:48:09 -0800

    Updated resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ class Home extends Component {
                                 baseball, backetball, and football, as well as keeping in shape by running and going to the gym. 
                             <br /><br />
                                 <p>
-                                    <a href="https://drive.google.com/file/d/1XqoorDzArVGlLLEmASeQ_Y-PpfOY3NF0/view?usp=sharing" target="_blank"> My Resume </a>
+                                    <a href="https://drive.google.com/file/d/1YpsUWaT6cBrFBQp9Ge4BZdWvaWrHg8dm/view?usp=sharing" target="_blank"> My Resume </a>
                                 </p>
                             </p>
                     </div>
