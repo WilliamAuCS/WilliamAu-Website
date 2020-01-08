222a222255f71cbd0bf1cc5a1527598e34cba23a
William Au <WilliamAuCS@gmail.com>
2020-01-08 11:51:49 -0800

    Grammer changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,14 +21,14 @@ class Home extends Component {
                                 It is a great time.
                             <br /><br />
                                 With technology at its peak, great developers are on the rise. Having multiple programming languages, libraries, 
-                                and frameworks at our desposal, we are able to create what people 20 years ago thought to be impossible.
+                                and frameworks at our disposal, we are able to create what people 20 years ago thought to be impossible.
                             <br /><br />
                                 My name is William Au and I am a senior in Computer Science. My passion for mathematics and technology led me to my
                                 interest in computer science. I enjoy solving problems analytically and in the most efficient way possible. 
-                                Programming is a fun pastime and also a great way to make a difference.
+                                Programming is a fun pastime as well as a great way to make a difference.
                             <br /> <br />
                                 In addition to programming and building computers, I also enjoy playing nearly all sports, including 
-                                baseball, backetball, and football, as well as keeping in shape by running and going to the gym. 
+                                baseball, basketball, and football, as well as keeping in shape by running and going to the gym. 
                             <br /><br />
                                 <p>
                                     <a href="https://drive.google.com/file/d/1YpsUWaT6cBrFBQp9Ge4BZdWvaWrHg8dm/view?usp=sharing" target="_blank"> My Resume </a>
